bcb694d110068a7a8100b02bba94d171ac22ced8
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-12-28 12:16:56 -0500

    added tests to TemplateCard

--- src/components/Template/TemplateCard.test.jsx
+++ src/components/Template/TemplateCard.test.jsx
@@ -1,7 +1,78 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import UserContext from '../../context/UserContext';
 import TemplateCard from './TemplateCard';
+import { notDeepEqual } from 'assert';
 
 it('renders without crashing', () => {
-  render(<TemplateCard />);
+  const currUser = { user: {
+    firstName: 'Jimmothy',
+    lastName: 'Deanus',
+    username: 'test',
+    password: '1234',
+    street: '60 Sierra Street',
+    city: 'Calumet City',
+    state: 'IL',
+    zip: '60409',
+    county: 'Cook',
+    email: 'jdean@gmail.com',
+    isAdmin: true,
+  }};
+
+  render(
+    <UserContext.Provider value={{currUser}}>
+      <TemplateCard />
+    </UserContext.Provider>);
+});
+
+it('shows user options if owned by the user', () => {
+  const currUser = { user: {
+    firstName: 'Jimmothy',
+    lastName: 'Deanus',
+    username: 'test',
+    password: '1234',
+    street: '60 Sierra Street',
+    city: 'Calumet City',
+    state: 'IL',
+    zip: '60409',
+    county: 'Cook',
+    email: 'jdean@gmail.com',
+    isAdmin: true,
+  }};
+
+  render(
+    <UserContext.Provider value={{currUser}}>
+      <TemplateCard userId='test' />
+    </UserContext.Provider>);
+
+  expect(
+    screen.getByRole("button", { name: "Edit" })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole("button", { name: "Delete" })
+  ).toBeInTheDocument();
+});
+
+it('shows no user options if not owned by the user', () => {
+  const currUser = { user: {
+    firstName: 'Jimmothy',
+    lastName: 'Deanus',
+    username: 'test',
+    password: '1234',
+    street: '60 Sierra Street',
+    city: 'Calumet City',
+    state: 'IL',
+    zip: '60409',
+    county: 'Cook',
+    email: 'jdean@gmail.com',
+    isAdmin: true,
+  }};
+
+  render(
+    <UserContext.Provider value={{currUser}}>
+      <TemplateCard userId='stranger' />
+    </UserContext.Provider>);
+  const buttonGroup = screen.queryByText('buttonGroup')
+  expect(buttonGroup).not.toBeInTheDocument()
+  
 });
