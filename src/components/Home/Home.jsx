7b12a31910b5eead91da99876a4054d85baae735
Bryce Beckenbach <brycebeckenbach@gmail.com>
2022-01-07 20:00:10 -0500

    added Home page component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <Container>
+        <div className="container text-center" style={{'marginTop': '20px', 'marginBottom': '20px'}}>
+            <h4>Created by <a target="_blank" rel="noopener noreferrer" href="https://brycebeckenbach.netlify.app/">Bryce Beckenbach</a></h4>
+            <div className="social-links-container">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/bryce-beckenbach-52a5276a/" style={{'marginLeft': '5px', 'marginRight': '5px', 'marginBottom': '5px'}}><i className="fab fa-linkedin fa-2x"></i></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/bbeckenb" style={{'marginLeft': '5px', 'marginRight': '5px', 'marginBottom': '5px'}}><i className="fab fa-github-square fa-2x"></i></a>
+                <a target="_blank" rel="noopener noreferrer" href="mailto:brycebeckenbach@gmail.com" style={{'marginLeft': '5px', 'marginRight': '5px', 'marginBottom': '5px'}}><i className="fas fa-envelope-square fa-2x"></i></a>
+            </div>
+        </div>
+      
+    </Container>
+  );
+}
+
+export default Home;
