c0daabfef896429a66be153b98888206aa797bc2
Bryce Beckenbach <brycebeckenbach@gmail.com>
2022-01-14 18:02:43 -0500

    added keys to option values for PostEditForm component

--- src/components/Post/PostCreateForm.jsx
+++ src/components/Post/PostCreateForm.jsx
@@ -81,58 +81,58 @@ const PostCreateForm = function ({ addPost }) {
                     type="select"
                     {...register('location')}
                     className={`form-control ${errors.location ? 'is-invalid' : ''}`}>
-                      <option value={null}>None</option>
-                      <option value="AL">Alabama</option>
-                      <option value="AK">Alaska</option>
-                      <option value="AZ">Arizona</option>
-                      <option value="AR">Arkansas</option>
-                      <option value="CA">California</option>
-                      <option value="CO">Colorado</option>
-                      <option value="CT">Connecticut</option>
-                      <option value="DE">Delaware</option>
-                      <option value="DC">District Of Columbia</option>
-                      <option value="FL">Florida</option>
-                      <option value="GA">Georgia</option>
-                      <option value="HI">Hawaii</option>
-                      <option value="ID">Idaho</option>
-                      <option value="IL">Illinois</option>
-                      <option value="IN">Indiana</option>
-                      <option value="IA">Iowa</option>
-                      <option value="KS">Kansas</option>
-                      <option value="KY">Kentucky</option>
-                      <option value="LA">Louisiana</option>
-                      <option value="ME">Maine</option>
-                      <option value="MD">Maryland</option>
-                      <option value="MA">Massachusetts</option>
-                      <option value="MI">Michigan</option>
-                      <option value="MN">Minnesota</option>
-                      <option value="MS">Mississippi</option>
-                      <option value="MO">Missouri</option>
-                      <option value="MT">Montana</option>
-                      <option value="NE">Nebraska</option>
-                      <option value="NV">Nevada</option>
-                      <option value="NH">New Hampshire</option>
-                      <option value="NJ">New Jersey</option>
-                      <option value="NM">New Mexico</option>
-                      <option value="NY">New York</option>
-                      <option value="NC">North Carolina</option>
-                      <option value="ND">North Dakota</option>
-                      <option value="OH">Ohio</option>
-                      <option value="OK">Oklahoma</option>
-                      <option value="OR">Oregon</option>
-                      <option value="PA">Pennsylvania</option>
-                      <option value="RI">Rhode Island</option>
-                      <option value="SC">South Carolina</option>
-                      <option value="SD">South Dakota</option>
-                      <option value="TN">Tennessee</option>
-                      <option value="TX">Texas</option>
-                      <option value="UT">Utah</option>
-                      <option value="VT">Vermont</option>
-                      <option value="VA">Virginia</option>
-                      <option value="WA">Washington</option>
-                      <option value="WV">West Virginia</option>
-                      <option value="WI">Wisconsin</option>
-                      <option value="WY">Wyoming</option>
+                      <option key="0" value={null}>None</option>
+                      <option key="AL" value="AL">Alabama</option>
+                      <option key="AK" value="AK">Alaska</option>
+                      <option key="AZ" value="AZ">Arizona</option>
+                      <option key="AR" value="AR">Arkansas</option>
+                      <option key="CA" value="CA">California</option>
+                      <option key="CO" value="CO">Colorado</option>
+                      <option key="CT" value="CT">Connecticut</option>
+                      <option key="DE" value="DE">Delaware</option>
+                      <option key="DC" value="DC">District Of Columbia</option>
+                      <option key="FL" value="FL">Florida</option>
+                      <option key="GA" value="GA">Georgia</option>
+                      <option key="HI" value="HI">Hawaii</option>
+                      <option key="ID" value="ID">Idaho</option>
+                      <option key="IL" value="IL">Illinois</option>
+                      <option key="IN" value="IN">Indiana</option>
+                      <option key="IA" value="IA">Iowa</option>
+                      <option key="KS" value="KS">Kansas</option>
+                      <option key="KY" value="KY">Kentucky</option>
+                      <option key="LA" value="LA">Louisiana</option>
+                      <option key="ME" value="ME">Maine</option>
+                      <option key="MD" value="MD">Maryland</option>
+                      <option key="MA" value="MA">Massachusetts</option>
+                      <option key="MI" value="MI">Michigan</option>
+                      <option key="MN" value="MN">Minnesota</option>
+                      <option key="MS" value="MS">Mississippi</option>
+                      <option key="MO" value="MO">Missouri</option>
+                      <option key="MT" value="MT">Montana</option>
+                      <option key="NE" value="NE">Nebraska</option>
+                      <option key="NV" value="NV">Nevada</option>
+                      <option key="NH" value="NH">New Hampshire</option>
+                      <option key="NJ" value="NJ">New Jersey</option>
+                      <option key="NM" value="NM">New Mexico</option>
+                      <option key="NY" value="NY">New York</option>
+                      <option key="NC" value="NC">North Carolina</option>
+                      <option key="ND" value="ND">North Dakota</option>
+                      <option key="OH" value="OH">Ohio</option>
+                      <option key="OK" value="OK">Oklahoma</option>
+                      <option key="OR" value="OR">Oregon</option>
+                      <option key="PA" value="PA">Pennsylvania</option>
+                      <option key="RI" value="RI">Rhode Island</option>
+                      <option key="SC" value="SC">South Carolina</option>
+                      <option key="SD" value="SD">South Dakota</option>
+                      <option key="TN" value="TN">Tennessee</option>
+                      <option key="TX" value="TX">Texas</option>
+                      <option key="UT" value="UT">Utah</option>
+                      <option key="VT" value="VT">Vermont</option>
+                      <option key="VA" value="VA">Virginia</option>
+                      <option key="WA" value="WA">Washington</option>
+                      <option key="WV" value="WV">West Virginia</option>
+                      <option key="WI" value="WI">Wisconsin</option>
+                      <option key="WY" value="WY">Wyoming</option>
                     </select>
                   
                   <div className="invalid-feedback" role="alert">{errors.location?.message}</div>
@@ -147,8 +147,8 @@ const PostCreateForm = function ({ addPost }) {
                     type="select"
                     {...register('tag')}
                     className={`form-control ${errors.tag ? 'is-invalid' : ''}`}>
-                      <option value={null}>None</option>
-                      {category.map((cat) => <option value={cat}>{cat}</option>)}
+                      <option key="0" value={null}>None</option>
+                      {category.map((cat) => <option key={cat} value={cat}>{cat}</option>)}
                   </select>
                   
                   <div className="invalid-feedback" role="alert">{errors.tag?.message}</div>
