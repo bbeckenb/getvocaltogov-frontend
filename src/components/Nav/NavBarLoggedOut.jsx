2d2607fc33c7b294900e8ef9c724a1cb86bd71b0
Bryce Beckenbach <brycebeckenbach@gmail.com>
2022-02-18 13:42:21 -0500

    fixed navbar for loading user flow

--- src/components/Nav/NavBarLoggedOut.jsx
+++ src/components/Nav/NavBarLoggedOut.jsx
@@ -1,20 +1,26 @@
+import React, { useContext } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import UserContext from '../../context/UserContext';
 
 function NavBarLoggedOut() {
-   return (
-    <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-           <Nav.Link href="/posts">Posts</Nav.Link>
-           <Nav.Link href="/templates">Templates</Nav.Link>
-       </Nav>
-       <Nav>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/">Login</Nav.Link>
-            {/* <Nav.Link href="/login">Login</Nav.Link> */}
-            <Nav.Link href="/signup">Sign up</Nav.Link>
-        </Nav>
-    </Navbar.Collapse>
-   ) 
+    const { loadingUser } = useContext(UserContext);
+
+    return (
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="/posts">Posts</Nav.Link>
+                <Nav.Link href="/templates">Templates</Nav.Link>
+            </Nav>
+            <Nav>
+                <Nav.Link href="/about">About</Nav.Link>
+                {loadingUser ? 
+                <Nav.Link href="">Loading</Nav.Link> :
+                <><Nav.Link href="/">Login</Nav.Link>
+                <Nav.Link href="/signup">Sign up</Nav.Link></>
+                }
+            </Nav>
+        </Navbar.Collapse>
+    ) 
 }
 
 export default NavBarLoggedOut;
